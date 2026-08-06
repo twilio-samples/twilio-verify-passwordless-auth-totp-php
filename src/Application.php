3424a0bfc6ede83d69b704bc623895fef3128f4b
Matthew Setter <matthew@matthewsetter.com>
2026-08-06 11:14:41 +1000

    Initial commit to the project
    This commit contains just the core of the application with just the core
    routes defined. Calling any of the routes won't do anything other than,
    see an HTTP 200 OK status code returned.

---
+++ src/Application.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App as SlimApp;
+use Slim\Interfaces\RouteInterface;
+use Slim\Middleware\ContentLengthMiddleware;
+
+/**
+ * This class encapsulates the central Slim application,
+ * making it easier to create and test.
+ */
+final class Application
+{
+    public function __construct(private readonly SlimApp $app)
+    {
+        $app->add(new ContentLengthMiddleware());
+        $app->addBodyParsingMiddleware();
+        $app->addRoutingMiddleware();
+        $app->addErrorMiddleware(true, true, true);
+    }
+
+    /**
+     * setupRoutes sets up the application's routing table
+     */
+    public function setupRoutes(): void
+    {
+        $this->app->get('/', [$this, 'displayCreateTotpFactorForm']);
+        $this->app->post('/', [$this, 'processCreateTotpFactorForm']);
+        $this->app->get('/challenge', [$this, 'displayVerifyUserForm']);
+        $this->app->post('/challenge', [$this, 'processVerifyUserForm']);
+        $this->app->post('/token', [$this, 'showQRCodeForm']);
+        $this->app->post('/token', [$this, 'processQRCodeForm']);
+    }
+
+    /**
+     * getRoutes returns the application's current routes
+     *
+     * @return RouteInterface[]
+     */
+    public function getRoutes(): array
+    {
+        return $this->app->getRouteCollector()->getRoutes();
+    }
+
+    /**
+     * run launches the application
+     */
+    public function run(): void
+    {
+        $this->app->run();
+    }
+
+    /**
+     * This renders the form where the user can enter their username to set
+     * up TOTP-based 2FA.
+     */
+    public function displayCreateTotpFactorForm(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $response;
+    }
+
+    /**
+     * This processes the handleShowCreateNewFactorForm and creates a new
+     * TOTP factor with Twilio. On success, it redirects the user to
+     * handleCreateQRCode
+     */
+    public function processCreateTotpFactorForm(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $response;
+    }
+
+    /**
+     * This renders the form with the QR code that the user needs to scan
+     * to verify themselves.
+     *
+     * After scanning the QR code and retrieving the TOTP code, they can
+     * enter it into the form and click "Verify" to finish the verification
+     * process.
+     */
+    public function displayVerifyUserForm(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $response;
+    }
+
+    /**
+     * This processes the verify user form. It redirects the user to the
+     * showQRCodeForm on success.
+     */
+    public function processVerifyUserForm(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $response;
+    }
+
+    /**
+     * This renders a form with a QR code and a field for the the TOTP to
+     * be entered.
+     */
+    public function showQRCodeForm(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $response;
+    }
+
+    /**
+     * This processes the showQRCodeForm form and verifies the TOTP
+     * submitted in the showQRCodeForm.
+     */
+    public function processQRCodeForm(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $response;
+    }
+}
